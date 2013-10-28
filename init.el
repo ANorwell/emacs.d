e676572f9024449c6d4ba142d8f73368b7021ca6
Arron Norwell <anorwell@mdialog.com>
2013-10-28 10:29:19 -0400

    new emacs dir format

---
+++ init.el
@@ -0,0 +1,14 @@
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+(require 'pallet)
+(add-to-list 'load-path "~/.emacs.d/custom")
+;(add-to-list 'load-path "~/.emacs.d/other_paths")
+
+
+(load '00common-setup.el')
+(load '01projectile.el')
+
+
+;;ido-mode
+(require 'ido)
+(ido-mode t)
